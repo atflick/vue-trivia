6e2eff87030ab005a7a1484f4030f6b78f42158d
Andy Flickinger <andy@interactivestrategies.com>
2020-04-16 23:37:33 -0400

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase'
+
+const app = initializeApp({
+  apiKey: 'AIzaSyDhBAep0mMiuve54FRZyVH3oYXNmJbvjxs',
+  authDomain: 'vue-trivia-94d5e.firebaseapp.com',
+  databaseURL: 'https://vue-trivia-94d5e.firebaseio.com',
+  projectId: 'vue-trivia-94d5e',
+  storageBucket: 'vue-trivia-94d5e.appspot.com',
+  messagingSenderId: '205718714727',
+  appId: '1:205718714727:web:31da878c4f608cd9b457c2',
+  measurementId: 'G-FNZGT92PXQ'
+})
+
+export const db = app.firestore()
+export const gamesCollection = db.collection('games')
